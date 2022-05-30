a70ca4bd0810393b301ad46e273a7acfacac6603
aleqxan <inno.alex@ymail.com>
2022-05-30 23:54:28 +0200

    Updated some file configuration

---
+++ pkg/shop/application/product.go
@@ -0,0 +1,35 @@
+package application
+
+type ProductReadModel interface {
+	AllProducts() ([]products.Product, err)
+}
+
+type ProductsService struct {
+	repo      products.Repository
+	readModel ProductReadModel
+}
+
+func NewProductsService() ProductsService {
+
+}
+
+func (s ProductsService) AllProducts() {
+
+}
+
+type AddProductCommand struct {
+	ID            string
+	Name          string
+	Description   string
+	PriceCents    uint
+	PriceCurrency string
+}
+
+func (s ProductsService) AddProduct(cmd AddProductCommand) error {
+
+	price.NewPrice(cmd.PriceCents, cmd.PriceCurrency)
+
+	products.NewProduct(products.ID(cmd.ID), cmd.Name, cmd.Description, price)
+
+	s.repo.Save
+}
---
+++ pkg/shop/infrastructure/products/memory.go
@@ -0,0 +1,21 @@
+package products
+
+import (
+
+)
+
+type MemoryRepository struct {
+
+}
+
+func Save{
+
+}
+
+func ByID{
+	
+}
+
+func AllProducts(){
+	
+}
